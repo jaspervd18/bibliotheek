560374f1410c02deaa0ee722514c6996f688d3ed
Jasper <jasper.vandyck@hotmail.be>
2023-06-02 16:20:13 +0200

    repositories aangepast aan nieuwe role

--- src/main/java/be/ewdj/bibliotheek/repository/UserRepository.java
+++ src/main/java/be/ewdj/bibliotheek/repository/UserRepository.java
@@ -4,10 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import be.ewdj.bibliotheek.models.UserEntity;
 
-import java.util.Optional;
-
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
-    Optional<UserEntity> findByUsername(String username);
+    UserEntity findByUsername(String username);
 
     Boolean existsByUsername(String username);
 }
