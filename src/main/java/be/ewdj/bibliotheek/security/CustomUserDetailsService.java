2310b20f18b82056d2008c68dbc6bb77550926f4
Jasper <jasper.vandyck@hotmail.be>
2023-06-02 16:20:27 +0200

    CustomUserDatilsService aangepast naar nieuwe Role

--- src/main/java/be/ewdj/bibliotheek/security/CustomUserDetailsService.java
+++ src/main/java/be/ewdj/bibliotheek/security/CustomUserDetailsService.java
@@ -1,6 +1,6 @@
 package be.ewdj.bibliotheek.security;
 
-import be.ewdj.bibliotheek.models.Role;
+import be.ewdj.bibliotheek.models.Roles;
 import be.ewdj.bibliotheek.models.UserEntity;
 import be.ewdj.bibliotheek.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,8 +13,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Collections;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
@@ -28,12 +27,12 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        UserEntity user = userRepository.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("Username not found"));
-        return new User(user.getUsername(), user.getPassword(), mapRolesToAuthorities(user.getRoles()));
+        UserEntity user = userRepository.findByUsername(username);
+        System.out.println(user);
+        return new User(user.getUsername(), user.getPassword(), mapRolesToAuthorities(user.getRole()));
     }
 
-    private Collection<GrantedAuthority> mapRolesToAuthorities(List<Role> roles) {
-        return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
+    private Collection<GrantedAuthority> mapRolesToAuthorities(Roles role) {
+        return Collections.singletonList(new SimpleGrantedAuthority(role.toString()));
     }
 }
