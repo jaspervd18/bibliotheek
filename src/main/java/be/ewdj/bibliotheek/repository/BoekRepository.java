0da6ac514517d0d079248d828283adb3a17aab91
Jasper <jasper.vandyck@hotmail.be>
2023-06-03 13:45:29 +0200

    repo geeft Optionals terug

--- src/main/java/be/ewdj/bibliotheek/repository/BoekRepository.java
+++ src/main/java/be/ewdj/bibliotheek/repository/BoekRepository.java
@@ -6,19 +6,19 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import be.ewdj.bibliotheek.models.Auteur;
 import be.ewdj.bibliotheek.models.Boek;
 
 public interface BoekRepository extends JpaRepository<Boek, Long> {
 
   Boek findById(long id);
 
-  @Query("SELECT b FROM Boek b WHERE b.isbn = ?1")
-  Boek findByIsbn(String isbn);
+  Optional<Boek> findByIsbn(String isbn);
 
   @Query("SELECT b FROM Boek b ORDER BY b.aantalSterren DESC, b.titel ASC LIMIT 10")
   List<Boek> findMostPopular();
 
   @Query("SELECT b FROM Boek b JOIN b.auteurs a WHERE a.naam LIKE ?1 OR a.voornaam LIKE ?1")
-  List<Boek> findBooksByAuthor(String auteur);
+  List<Boek> findBooksByAuthor(Auteur auteur);
 
 }
